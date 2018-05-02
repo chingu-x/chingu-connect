7bdc7cc01e375faa8b367cb9439be6a3e29a8fce
The-Vampiire <vampiirecodes@gmail.com>
2018-05-02 02:52:50 -0400

    Moved singular queries above plural queries

--- graphql/connection/queries.js
+++ graphql/connection/queries.js
@@ -5,17 +5,17 @@ const ConnectionType = require('./connectionType');
 const { GraphQLList, GraphQLID, GraphQLNonNull } = graphql;
 
 module.exports = {
-  connections: {
-    type: new GraphQLList(ConnectionType),
-    resolve() {
-      return Connection.find({});
-    },
-  },
   connection: {
     type: ConnectionType,
     args: { id: { type: new GraphQLNonNull(GraphQLID) } },
     resolve(parentValue, { id }) {
       return Connection.findById(id);
+    },
+  },
+  connections: {
+    type: new GraphQLList(ConnectionType),
+    resolve() {
+      return Connection.find({});
     },
   },
 };
--- graphql/user/queries.js
+++ graphql/user/queries.js
@@ -5,17 +5,17 @@ const UserType = require('./userType');
 const { GraphQLList, GraphQLID, GraphQLNonNull } = graphql;
 
 module.exports = {
-  users: {
-    type: new GraphQLList(UserType),
-    resolve() {
-      return User.find({});
-    },
-  },
   user: {
     type: UserType,
     args: { id: { type: new GraphQLNonNull(GraphQLID) } },
     resolve(parentValue, { id }) {
       return User.findById(id);
+    },
+  },
+  users: {
+    type: new GraphQLList(UserType),
+    resolve() {
+      return User.find({});
     },
   },
 };
