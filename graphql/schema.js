6f1790fdd12512904289af3ed283b041550d66ed
The-Vampiire <vampiirecodes@gmail.com>
2018-05-03 20:24:34 -0400

    Added notes, clarified naming

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,12 +1,12 @@
 const { join } = require('path');
 const { importSchema } = require('graphql-import'); // imports .graphql files
 const { makeExecutableSchema } = require('graphql-tools'); // combines type defs and resolvers
-const userQueries = require('./user/queries');
+const userResolvers = require('./user/resolvers');
 
 const typeDefs = importSchema(join(__dirname, 'schema.graphql'));
 
 const resolvers = {
-  ...userQueries,
+  ...userResolvers,
 };
 
 module.exports = makeExecutableSchema({ typeDefs, resolvers });
---
+++ graphql/user/resolvers.js
@@ -0,0 +1,30 @@
+// Defines the Type: Query, Mutation, and reference Type resolvers
+
+/**
+ * Startup Errors:
+ * Error: "Mutation/Query/*" defined in resolvers, but not in schema
+ *   "*": defined in JS resolver file but not in Type schema
+ *
+ * Syntax Error: Expected Name, found }
+ *   empty Type definition in Type schema
+ *
+ * Runtime Errors:
+ * It is possible to write definitions in the Type schema and not have the resolvers to support it
+ *   this can be exploited to establish the schema contract so dev teams can break off for work
+ *
+ * It is possible to write mismatched definitions between the Type schema and the resolver file
+ *   the GraphiQL client will display what the Type schema defines
+ */
+
+const User = require('../../db/user');
+
+module.exports = {
+  Query: {
+    user: id => User.findById(id),
+    users: () => User.find({}),
+  },
+  // Mutation: { },
+  User: {
+    // reference Type resolvers
+  },
+};
