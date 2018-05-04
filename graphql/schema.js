0eac2fae8847b26b91aa5f99387f84786b07422d
The-Vampiire <vampiirecodes@gmail.com>
2018-05-03 21:34:05 -0400

    Added notes

--- graphql/schema.js
+++ graphql/schema.js
@@ -11,12 +11,25 @@ const typeDefs = importSchema(join(__dirname, 'schema.graphql'));
 const resolvers = {
   User,
   Connection,
-  Query: {
+  Query: { // RootQuery
     ...userQueries,
     ...connectionQueries,
   },
-  // Mutation: {},
+  // Mutation: { // RootMutation
+    // ...userMutations,
+    // ...connectionMutations,
+  // },
 };
 
 module.exports = makeExecutableSchema({ typeDefs, resolvers });
+
+/**
+ * Pattern:
+ * ------------------
+ * Root Query and Mutation must exist in BOTh the schema.graphql AND schema.js
+ * each Type must also use the Query and Mutation object for their own resolvers
+ *   these are renamed in schema.js so they can be spread
+ *   into the RootQuery and RootMutation that share the same name
+ * TODO: look for a way to perform the same rename + spread in schema.graphql
+ */
 
