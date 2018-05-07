dce0ed3f1400e61a1c969dcbf654c12928ccacdb
The-Vampiire <vampiirecodes@gmail.com>
2018-05-07 15:45:18 -0400

    fixed naming

--- db/__tests__/user.test.js
+++ db/__tests__/user.test.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const { User } = require('../user');
-const { UserMock } = require('../__mocks__/User');
+const { UserMock } = require('../__mocks__/user');
 
 mongoose.Promise = global.Promise;
 
