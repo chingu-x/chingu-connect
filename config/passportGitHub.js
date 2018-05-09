9b581d4005b3aeff3195a8b7d7c70674abc578ad
The-Vampiire <vampiirecodes@gmail.com>
2018-05-09 02:14:11 -0400

    added jsDocs

--- config/passportGitHub.js
+++ config/passportGitHub.js
@@ -2,6 +2,31 @@ const passport = require('passport');
 const GithubStrategy = require('passport-github2').Strategy;
 const { User } = require('../db/user/user');
 
+/**
+ * Callback function for GitHub oAuth handshake in Passport
+ *
+ * Finds or creates an authenticated user
+ *
+ * @param {string} accessToken ignored
+ * @param {string} refreshToken ignored
+ * @param {Object} githubData destructured username, ID, and avatar URL
+ * @callback done Passport callback: authenticated user
+ */
+const authCallback = async (
+  accessToken,
+  refreshToken,
+  { id, _json: { login, avatar_url } },
+  done,
+) => {
+  try {
+    const user = (
+      await User.findOne({ githubID: id }) ||
+      await new User({ githubID: id, username: login, avatar: avatar_url }).save()
+    );
+    return done(null, user);
+  } catch (error) { return done(error, null); }
+};
+
 // define the strategy to be used by this passport endpoint
 const strategy = new GithubStrategy(
   {
@@ -10,34 +35,33 @@ const strategy = new GithubStrategy(
     callbackURL: process.env.GITHUB_AUTH_CALLBACK_URL,
     scope: 'read:user',
   },
-  async (
-    accessToken,
-    refreshToken,
-    { id, _json: { login, avatar_url } },
-    done,
-  ) => {
-    try {
-      const user = (
-        await User.findOne({ githubID: id }) ||
-        await new User({ githubID: id, username: login, avatar: avatar_url }).save()
-      );
-      return done(null, user);
-    } catch (error) { return done(error, null); }
-  },
+  authCallback,
 );
 
-// define serializer / deserializer
-passport.serializeUser((user, done) => done(null, user.id));
-passport.deserializeUser(
-  (userID, done) => {
-    User.findById(userID)
-    .then((user) => {
-      if (user) return done(null, user);
-      return done('Authentication failed. User not found', null);
-    });
-  },
-);
+/**
+ * Serializes a user ID to be stored in the coooookie
+ * @param {string} user User instance
+ * @callback Passport callback: serialized user ID
+ */
+const serializeUser = (user, done) => done(null, user.id);
 
+/**
+ * Deserializes a user ID to find the authenticated user
+ * @param {string} userID user ID deserialized from the cookie
+ * @callback Passport callback: found user
+ */
+const deserializeUser = (userID, done) => {
+  User.findById(userID)
+  .then((user) => {
+    if (user) return done(null, user);
+    return done('Authentication failed. User not found', null);
+  });
+};
+
+passport.serializeUser(serializeUser);
+passport.deserializeUser(deserializeUser);
+
+// enable the configuration
 passport.use(strategy);
 
 // router is being used as a stand-in for app.js so config can be done in this file
