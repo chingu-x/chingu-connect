9dfa11094c4555955b292cdbc2ecd9d890a42880
The-Vampiire <vampiirecodes@gmail.com>
2018-05-09 21:01:40 -0400

    TESTDB IS BACK BITCH

--- test_utils/index.js
+++ test_utils/index.js
@@ -1,7 +1,9 @@
 const mockData = require('./mock_data');
 const compareArrs = require('./compareArrs');
+const TestDB = require('./testDB');
 
 module.exports = {
   mockData,
   compareArrs,
+  TestDB,
 };
