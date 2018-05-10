a3d73b8940056aa00cbb1a95feaa2ce4c64e79f8
The-Vampiire <vampiirecodes@gmail.com>
2018-05-09 20:25:19 -0400

    RIP test server && db

--- test_utils/index.js
+++ test_utils/index.js
@@ -1,11 +1,7 @@
-const testDB = require('./testDB');
-const testServer = require('./testServer');
 const mockData = require('./mock_data');
 const compareArrs = require('./compareArrs');
 
 module.exports = {
-  testDB,
-  testServer,
   mockData,
   compareArrs,
 };
