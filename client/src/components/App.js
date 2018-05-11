2d1138c2efca286f691b91631ae73ece07cca2fe
ziggysauce <dtn3423@gmail.com>
2018-05-11 15:12:08 -0400

    Add initial components and styling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const App = () => (
   <div className="app-container">
-    <h1>Chingu Connect</h1>
-    <p>A tool to optimally connect learners. Designed serendipity</p>
+    <div className="app-card">
+        <h1>Chingu Connect</h1>
+        <p>A tool to optimally connect learners. Designed serendipity</p>
+      </div>
   </div>
 );
 
