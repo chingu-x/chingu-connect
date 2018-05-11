a766f1d674a2c6523b96059107d8b4a576b77b8d
ziggysauce <dtn3423@gmail.com>
2018-05-11 15:55:36 -0400

    add port 8080 to cors, add grpahql query example

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
 const App = () => (
   <div className="app-container">
+    <Query
+      query={gql`
+        {
+          users {
+            id
+          }
+        }
+      `}
+    >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error!</p>;
+      console.log('USERS: ', data.users);
+      return null;
+    }}
+    </Query>
+
     <div className="app-card">
         <h1>Chingu Connect</h1>
         <p>A tool to optimally connect learners. Designed serendipity</p>
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
 import App from './components/App';
 import './styles/main.scss';
 
+// uri defaults to `/graphql`
+const client = new ApolloClient({
+  uri: 'http://localhost:8008/graphql',
+});
+
 ReactDOM.render(
-  <AppContainer>
-    <App />
-  </AppContainer>,
+  <ApolloProvider client={client}>
+    <AppContainer>
+      <App />
+    </AppContainer>
+  </ApolloProvider>,
   document.getElementById('root'),
 );
 
@@ -15,9 +24,11 @@ ReactDOM.render(
 if (module.hot) {
   module.hot.accept('./components/App', () => {
     ReactDOM.render(
-      <AppContainer>
-        <App />
-      </AppContainer>,
+      <ApolloProvider client={client}>
+        <AppContainer>
+          <App />
+        </AppContainer>
+      </ApolloProvider>,
       document.getElementById('root'),
     );
   });
