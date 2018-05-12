9ff332aefbee3156f265df46ef61b14e7be91261
The-Vampiire <vampiirecodes@gmail.com>
2018-05-12 13:51:41 -0400

    finished Connection resolver tests

--- graphql/connection/resolvers.js
+++ graphql/connection/resolvers.js
@@ -1,15 +1,23 @@
 const { Connection } = require('../../db');
 
+const getConnection = (root, { input: { id } }) => Connection.findById(id);
+const getConnections = () => Connection.find({});
+
+const getOwner = connection => connection.getOwner();
+const getPartner = connection => connection.getPartner();
+
 module.exports = {
+  getConnection,
+  getConnections,
   // reference / custom Type resolvers for Connection Type
   Connection: {
-    owner: connection => connection.getOwner(),
-    partner: connection => connection.getPartner(),
+    owner: getOwner,
+    partner: getPartner,
   },
 
   Query: {
-    connection: (root, { input: { id } }) => Connection.findById(id),
-    connections: () => Connection.find({}),
+    connection: getConnection,
+    connections: getConnections,
   },
 
   // Mutation: {}
