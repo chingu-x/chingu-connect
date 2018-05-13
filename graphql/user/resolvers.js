28c3991fc53c02c162a0b31cc6beb5322ed0fdd4
The-Vampiire <vampiirecodes@gmail.com>
2018-05-13 05:53:48 -0400

    refactored User Type resolvers to use context. added a shitload of notes

--- graphql/user/resolvers.js
+++ graphql/user/resolvers.js
@@ -16,31 +16,53 @@
  *   the GraphiQL client will display what the Type schema defines
  */
 
-const { User } = require('../../db');
+ // a Type field resolver is the perfect place for using the 'root' parameter
+ // it come in the form of a model instance for the corresponding Type
+ // in this case the User database model is passed as a 'user' instance
+ // now it makes  sense why the root param is first despite being unused in most other resolvers
+  // it makes writing Type field resolvers clean one liners
+  // especially when combined with a modern ORM like mongoose
+  // mongoose provides instance methods that further support the simplistic declarative syntax
 
 const getCreated = user => user.ownedConnections();
 const getJoined = user => user.joinedConnections();
 
 const getUser = (
   root,
-  { input: { id, githubID, username } }, // deconstructs the UserInput object parameter
+  // Input Type objects are versatile because specific fields can be
+    // hand selected through deconstruction in the resolvers
+  { input: { id, githubID, username } }, // deconstructs the UserInput object - only the applicable fields are taken
+  { models: { User } }, // deconstructs the context object passed in app.js graphqlExpress middleware
 ) => {
-  if (id) return User.findById(id);
-  else if (githubID) return User.findOne({ githubID });
-  else if (username) return User.findOne({ username });
-  return null;
+  // consider the order and reasoning when designing conditional chains - they are not random
+  if (id) return User.findById(id); // the id (_id) is a unique indexed key of a User document in the db collection - fastest lookup
+  else if (githubID) return User.findOne({ githubID }); // also unique, not indexed, but preferred as an immutable reference
+  else if (username) return User.findOne({ username }); // future abilities to change usernames makes this the last resort
+  return null; // since we cant rely on the implicit null returned by a failed find* lookup we have to explicitly return it ourselves
 };
 
-const getUsers = () => User.find({});
+// (preference) in a case where you only need the third parameter
+// write out the default names for clarity and consistency
+const getUsers = (root, args, { models: { User } }) => User.find({});
 
 module.exports = {
+  // exported for unit tests
   getUser,
   getUsers,
-  // reference / custom Type resolvers for User Type
+
+  // reference / custom Type field resolvers for User Type must be exported
+  // if you change or the names of fields in the gql Type Definition or model Schema
+    // you have to resolve them manually and export them as well
+  // primitive type fields / field names that mirror the model Schema
+    // do not need to be written, they are automatically mapped
   User: {
     created: getCreated,
     joined: getJoined,
   },
+
+  // this is a way of modularizing the Root Query
+  // so the resolvers can all be housed in one file / directory (single source)
+  // in schema.js they are combined into a single Root Query using the spread operator
   Query: {
     user: getUser, // User
     users: getUsers, // [User]
@@ -48,3 +70,9 @@ module.exports = {
 
   // Mutation: { },
 };
+
+// ** AS THE SCHEMA GROWS: MODULARIZING THE CODE ** //
+// as a Type Definition grows (especially with regard to Mutations) it is worth considering
+// breaking up the resolvers into modules under the Type directory
+  // ex: '/user/resolvers/': query.js, mutation.js, field.js, index.js (for exporting)
+  // tests could also be housed in this dir
